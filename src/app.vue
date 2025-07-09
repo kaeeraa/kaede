02198a0dd6d5c63e0592605ce813bbc11b791f0c
fractal <fractal@nebula-nook.ru>
2025-07-09 15:54:51 +0300

    feat: use tauri api in js

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,12 @@
 <template>
   <NuxtLayout name="root">
-    asd
+    {{ configurationFilePath }}
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
+import { join, appConfigDir } from "@tauri-apps/api/path";
+
+const appConfigDirectoryPath = await appConfigDir();
+const configurationFilePath = await join(appConfigDirectoryPath, "config.json5");
 </script>
