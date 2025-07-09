53a791d497ad5bb47caf6a94af86620539f53266
fractal <fractal@nebula-nook.ru>
2025-07-09 16:29:13 +0300

    feat!: add a bunch of plugins for tauri

--- src/app.vue
+++ src/app.vue
@@ -6,7 +6,14 @@
 
 <script setup lang="ts">
 import { join, appConfigDir } from "@tauri-apps/api/path";
+import { sendNotification } from "@tauri-apps/plugin-notification";
+import {  } from "@tauri-apps/plugin-cli";
 
 const appConfigDirectoryPath = await appConfigDir();
 const configurationFilePath = await join(appConfigDirectoryPath, "config.json5");
+
+sendNotification({
+    title: "Kaede",
+    body:  "sosal?",
+});
 </script>
