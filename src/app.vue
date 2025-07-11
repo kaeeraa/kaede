db6d69bbc30fa30b0297e712c2d0e73ba9827339
fractal <fractal@nebula-nook.ru>
2025-07-11 17:18:10 +0300

    feat: make webview window acrylic effected

--- src/app.vue
+++ src/app.vue
@@ -9,11 +9,12 @@
 <script setup lang="ts">
 // reset all css styles in the tailwind style
 import "@unocss/reset/tailwind.css";
-import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
+import {getCurrentWebviewWindow} from "@tauri-apps/api/webviewWindow";
 import initializeConfigFile from "~/lib/helpers/initializeConfigFile";
 import makeConfigsDirectory from "~/lib/helpers/makeConfigsDirectory";
-import { useApplicationConfiguration, useApplicationInfo } from "~/lib/stores/app";
+import {useApplicationConfiguration, useApplicationInfo} from "~/lib/stores/app";
 import initializeInstancesFiles from "~/lib/helpers/initializeInstancesFiles";
+import {Effect} from "@tauri-apps/api/window";
 
 // get Tauri's webview window
 const currentWebview = getCurrentWebviewWindow();
@@ -49,6 +50,11 @@ shouldUseCustomTitleBar
   // here we manually set it to "Kaede v{version}"
   // (only needed for system title bar, so we run it here conditionally)
   : await currentWebview.setTitle(`Kaede v${appInfoStore.version}`);
+
+// temporarily set window effect
+await currentWebview.setEffects({
+  effects: [Effect.Acrylic],
+});
 
 // tauri doesn't wait for frontend to load and launches webview2 with flashing blank white screen.
 // this is why webview window is initially not visible in the tauri.config.json, so that
--- src/components/layout/Titlebar.vue
+++ src/components/layout/Titlebar.vue
@@ -1,8 +1,11 @@
 <template>
   <!-- "data-tauri-drag-region" here to tell tauri that -->
   <!-- this area should be draggable -->
-  <div data-tauri-drag-region class="z-10000 h-8 w-full sticky top-0">
-    {{ store.name }}
+  <div data-tauri-drag-region class="z-10000 h-8 w-full sticky top-0 flex justify-between bg-black bg-opacity-60">
+    <div class="text-white">
+d
+    </div>
+    <div></div>
   </div>
 </template>
 
