902a133911b4bf673c6bf36de1a61e912ecd93a2
fractal <fractal@nebula-nook.ru>
2025-07-19 18:20:08 +0300

    feat(nix): add nix support
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Development environment for Tauri project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gobject-introspection
+            cargo
+            cargo-tauri
+            nodejs
+          ];
+
+          buildInputs = with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+          ];
+        };
+      });
+}
