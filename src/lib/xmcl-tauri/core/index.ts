01bd45f100975b861e59d044d836184c4023897f
fractal <fractal@nebula-nook.ru>
2025-09-02 23:48:40 +0300

    feat: add initial code for xmcl rewrite

---
+++ src/lib/xmcl-tauri/core/index.ts
@@ -0,0 +1,6 @@
+export * from "./launch";
+export * from "./version";
+export * from "./platform";
+export * from "./folder";
+export * from "./diagnose";
+export { checksum } from "./utilities.ts";
