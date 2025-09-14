f357e82570e9770b02e187d5b85d144ac15e46d8
fractal <fractal@nebula-nook.ru>
2025-09-14 18:38:45 +0300

    chore: he just said me to do that
    Signed-off-by: Kaeeraa <kaeeraa@nebula-nook.ru>

--- src/lib/xmcl-tauri/core/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./launch";
-export * from "./version";
-export * from "./platform";
-export * from "./folder";
-export * from "./diagnose";
-export { checksum } from "./utilities.ts";
