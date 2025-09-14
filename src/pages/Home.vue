c119ab58ca2e5f30f05aeb3fbcdb18351af9f08c
fractal <fractal@nebula-nook.ru>
2025-09-14 18:59:14 +0300

    feat: add required things to start developing launch & download
    Signed-off-by: Kaeeraa <kaeeraa@nebula-nook.ru>

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 import { Routes } from "@/constants/routes";
 import { RouterLink } from "@kitbag/router";
+
+async function download() { /* TODO */ }
+async function run() { /* TODO */ }
 </script>
+
+// TODO: remove temp styles
+<style lang="css">
+  button {
+    margin: 3px;
+    background-color: lightgray;
+  }
+  a h1 h2 h3 h4 h5 h6 text {
+    background-color: aliceblue;
+  }
+</style>
 
 <template>
   <div>
     Home
   </div>
+  <button @click="download">uh, download</button>
+  <button @click="run">run</button>
   <RouterLink :to="(resolve) => resolve(Routes.About.Key)">
     Navigate to About
   </RouterLink>
