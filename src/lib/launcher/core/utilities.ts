6f01ee972eed65cc54b7941389f7664077fba2b9
fractal <fractal@nebula-nook.ru>
2025-09-28 00:00:58 +0300

    feat: implement checksum function
    Signed-off-by: Kaeeraa <kaeeraa@nebula-nook.ru>

--- src/lib/launcher/core/utilities.ts
+++ src/lib/launcher/core/utilities.ts
@@ -1,3 +1,15 @@
-export function checksum() {
-  return;
+import { readFile } from "@tauri-apps/plugin-fs";
+
+export async function checksum(path: string, hash: string): Promise<boolean> {
+  const file = await readFile(path);
+  const arrayBuffer = file.buffer.slice(
+    file.byteOffset,
+    file.byteOffset + file.byteLength,
+  );
+  const buffer = await crypto.subtle.digest("SHA-1", arrayBuffer);
+  const fileHash = [...new Uint8Array(buffer)]
+    .map(b => b.toString(16).padStart(2, "0"))
+    .join("");
+
+  return fileHash === hash;
 }
