8460862280a820e75911d65033a9089b6122c872
fractal <fractal@nebula-nook.ru>
2025-09-28 00:11:46 +0300

    feat: implement func for downloading artifact
    Signed-off-by: Kaeeraa <kaeeraa@nebula-nook.ru>

--- src/lib/launcher/core/download.ts
+++ src/lib/launcher/core/download.ts
@@ -1,3 +1,17 @@
+import type { Artifact } from "@/lib/schemas/minecrafts-schemas";
+import { download } from "@tauri-apps/plugin-upload";
+import { checksum, validateFileSize } from "./utilities";
+
+
 async function downloadVersionManifest(): Promise<Response> {
   return fetch("https://piston-meta.mojang.com/mc/game/version_manifest.json");
+}
+
+async function downloadArtifact(artifact: Artifact, prefix: string): Promise<boolean> {
+  const filePath = `${prefix}/${artifact.path}`;
+
+  await download(artifact.url, filePath);
+
+  return await validateFileSize(filePath, artifact.size)
+    && await checksum(filePath, artifact.sha1);
 }
