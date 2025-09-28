941e97a0a2437ba3ee646c847921e1415a19d2e8
fractal <fractal@nebula-nook.ru>
2025-09-28 12:38:58 +0300

    feat: declare SizeError class
    Signed-off-by: Kaeeraa <kaeeraa@nebula-nook.ru>

--- src/lib/launcher/core/errors.ts
+++ src/lib/launcher/core/errors.ts
@@ -10,3 +10,16 @@ export class ChecksumError extends Error {
     this.actual = actual;
   }
 }
+
+export class SizeError extends Error {
+  declare readonly path    : string;
+  declare readonly expected: string;
+  declare readonly actual  : string;
+
+  constructor(path: string, expected: string, actual: string) {
+    super(`Size mismatch for ${path}, expected ${expected}, got ${actual}`);
+    this.path = path;
+    this.expected = expected;
+    this.actual = actual;
+  }
+}
