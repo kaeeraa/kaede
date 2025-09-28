f7398d66568cb972f917e787493cbe5e944ca79c
fractal <fractal@nebula-nook.ru>
2025-09-28 12:44:47 +0300

    feat: make Tree consts for application
    Signed-off-by: Kaeeraa <kaeeraa@nebula-nook.ru>

--- src/constants/application.ts
+++ src/constants/application.ts
@@ -1,5 +1,14 @@
+import { BaseDirectory } from "@tauri-apps/plugin-fs";
+
 export const ApplicationName = "Kaede";
 export const ApplicationNamespace = "__KAEDE__";
 export const ApplicationRootID = "#app";
 export const ApplicationStopExecutionWord = "stop";
+
 export const ConfigFilename = "config.json";
+
+export const TreeResources = `${BaseDirectory.AppData}/resources`;
+export const TreeAssets = `${TreeResources}/assets`;
+export const TreeAssetIndexes = `${TreeAssets}/indexes`;
+export const TreeLibraries = `${TreeResources}/libraries`;
+export const TreeLogging = `${TreeResources}/logging`;
