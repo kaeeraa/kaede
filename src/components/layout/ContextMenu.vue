1ad6915b15fdafdf7e500cee7347d9e624a10173
fractal <fractal@nebula-nook.ru>
2025-10-26 13:18:33 +0300

    feat: implement a context menu

---
+++ src/components/layout/ContextMenu.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Ripple } from "m3ripple-vue";
+import { ApplicationNamespace } from "@/constants/application.ts";
+
+const { opened, x, y } = defineProps<{
+  "opened": boolean;
+  "x"     : number;
+  "y"     : number;
+}>();
+
+function restart(): void {
+  window.location.reload();
+}
+
+const rippleColor = window[ApplicationNamespace].variables.rippleColor;
+</script>
+
+<template>
+  <Transition name="pop">
+    <div
+      v-if="opened"
+      id="context_menu"
+      class="context_menu __context-menu-disable absolute z-50000 flex flex-col select-none bg-neutral-800 py-1 gap-1 text-white"
+      :style="{ left: `${x + 4}px`, top: `${y + 4}px` }"
+    >
+      <button
+        @click="restart"
+        class="context_menu_button __context-menu-disable relative flex flex-nowrap items-center gap-2 p-2"
+      >
+        <span class="__context-menu-disable block i-lucide-rotate-ccw size-4"></span>
+        <span class="__context-menu-disable text-sm block leading-none">
+          Restart
+        </span>
+        <Ripple :rippleColor="rippleColor" />
+      </button>
+    </div>
+  </Transition>
+</template>
--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -1,17 +1,79 @@
 <script setup lang="ts">
 import Sidebar from "@/components/layout/Sidebar.vue";
 import ErrorBoundary from "@/components/handlers/ErrorBoundary.vue";
+import { nextTick, ref } from "vue";
+import ContextMenu from "@/components/layout/ContextMenu.vue";
+import { useEventListener } from "@vueuse/core";
 
-function showContextMenu(): void {}
+const contextMenu = ref<{
+  "opened": boolean;
+  "x"     : number;
+  "y"     : number;
+}>({ "opened": false, "x": 0, "y": 0 });
+
+function closeContextMenu(): void {
+  contextMenu.value.opened = false;
+}
+function showContextMenu(event: MouseEvent): void {
+  const target = event.target as HTMLElement;
+
+  if (
+    target?.className?.includes("context_menu") ||
+    target?.parentElement?.className?.includes?.("context_menu_button")
+  ) {
+    return;
+  }
+
+  if (
+    target?.className?.includes?.("__context-menu-disable") ||
+    target?.className?.includes?.("_rippleOverlay")
+  ) {
+    closeContextMenu();
+
+    return;
+  }
+
+  if (contextMenu.value.opened) {
+    nextTick().then(() => {
+      contextMenu.value.opened = true;
+      contextMenu.value.x = event.clientX;
+      contextMenu.value.y = event.clientY;
+    });
+
+    return;
+  }
+
+  contextMenu.value.opened = true;
+  contextMenu.value.x = event.clientX;
+  contextMenu.value.y = event.clientY;
+}
+
+useEventListener(window, "mousedown", (event: MouseEvent) => {
+  const target = event.target as HTMLElement;
+
+  if (
+    target?.className?.includes("context_menu") ||
+    target?.parentElement?.className?.includes?.("context_menu_button")
+  ) {
+    return;
+  }
+
+  closeContextMenu();
+});
 </script>
 
 <template>
   <div
     @contextmenu.prevent
     @contextmenu="showContextMenu"
-    class="flex flex-nowrap gap-0"
+    class="flex flex-nowrap gap-0 relative w-full h-vh overflow-hidden"
   >
     <div id="__kaede-extensions" class="block" />
+    <ContextMenu
+      :opened="contextMenu.opened"
+      :x="contextMenu.x"
+      :y="contextMenu.y"
+    />
     <Sidebar />
     <!-- Pages error boundary -->
     <ErrorBoundary>
