fb110acb5873da49ff664b200e63cc7902a8d9ce
fractal <fractal@nebula-nook.ru>
2025-10-27 12:38:11 +0300

    feat: use a virtualized list to display logs

--- src/components/logging/LogViewer.vue
+++ src/components/logging/LogViewer.vue
@@ -1,37 +1,62 @@
 <script setup lang="ts">
+import { Ripple } from "m3ripple-vue";
 import { inject } from "vue";
 import type { GlobalStatesChangerType } from "@/types/application/global-states.type.ts";
 import { ApplicationNamespace, GlobalStatesChangerContextKey } from "@/constants/application.ts";
+import { VirtualisedList } from "vue-virtualised";
 
 const changeGlobalStates = inject<GlobalStatesChangerType>(GlobalStatesChangerContextKey);
 
 function showContextMenu(event: MouseEvent): void {
   window[ApplicationNamespace].functions.showContextMenu(event);
 }
+function closeLogViewer(): void {
+  changeGlobalStates?.("showLogs", false);
+}
 
-console.log(changeGlobalStates);
+const rippleColor = window[ApplicationNamespace].variables.rippleColor;
+const windowHeight = window.innerHeight;
 </script>
 
 <template>
-  <div class="pointer-events-none absolute bottom-0 left-0 right-0 top-0 grid place-items-center">
+  <div
+    @click="closeLogViewer"
+    class="absolute bottom-0 left-0 right-0 top-0 grid place-items-center bg-[theme(colors.black/.2)]"
+  >
     <div
+      @click.stop
       @contextmenu.prevent
       @contextmenu="showContextMenu"
       class="pointer-events-auto z-40000 h-fit max-h-[calc(100vh-64px)] max-w-[calc(100vw-64px)] w-fit flex flex-col gap-2 bg-neutral-900 p-4 text-white drop-shadow-lg"
     >
-      <div class="flex flex-nowrap w-full justify-between items-center gap-4">
-        <p class="select-none text-lg font-medium leading-none">
+      <div class="w-full flex flex-nowrap items-center justify-between gap-4">
+        <p class="select-none text-xl font-medium leading-none">
           Logs
         </p>
         <button
-          @click="() => changeGlobalStates?.('showLogs', false)"
+          class="relative rounded-full p-1"
+          @click="closeLogViewer"
         >
-          <span class="size-5 block i-lucide-x"></span>
+          <span class="i-lucide-x block size-5"></span>
+          <Ripple :rippleColor="rippleColor" />
         </button>
       </div>
-      <p class="select-none text-sm text-neutral-300">
+      <p class="select-none text-neutral-300">
         View current Kaede and Minecraft logs
       </p>
+      <div class="relative max-w-200 w-[calc(100vw-128px)] overflow-auto border border-neutral-300 bg-neutral-800 text-sm font-mono">
+        <VirtualisedList
+          :get-node-height="() => 20"
+          :viewport-height="windowHeight - 168"
+          :nodes="[]"
+        >
+          <template #cell="slotProps">
+            <p class="px-2">
+              {{ slotProps.node }}
+            </p>
+          </template>
+        </VirtualisedList>
+      </div>
     </div>
   </div>
 </template>
