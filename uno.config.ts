e53c69ffb88df4f13dba6b54a3180692ab318aaf
fractal <fractal@nebula-nook.ru>
2025-11-10 22:20:26 +0300

    style: improve code style

--- uno.config.ts
+++ uno.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   "presets": [
 
     /*
-     * 'presetWind4' requires Chrome 111+ (March 9, 2023)
+     * 'presetWind4' requires Chrome 111+ (March 9, 2023).
      * Otherwise colors won't work, so not worth it
      */
     presetWind3({
