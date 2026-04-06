7c88f2fac1c509b742e19c20cee07445299d522a
fractal <fractal@nebula-nook.ru>
2026-04-06 21:00:58 +0300

    refactor: improve hashString util and properly reference it

--- src/lib/general/scopes/hash-string.ts
+++ src/lib/general/scopes/hash-string.ts
@@ -1,12 +1,14 @@
+/*
+ * Source - https://stackoverflow.com/a/65239086
+ * Posted by amirhe, modified by community. See post 'Timeline' for change history
+ * Retrieved 2026-04-06, License - CC BY-SA 4.0
+ */
 export function hashString(input: string): number {
   let hash: number = 0;
 
   for (let index = 0; index < input.length; index++) {
-    hash = Math.trunc(
-      Math.imul(31, hash) +
-      (input.codePointAt(index) ?? 0),
-    );
+    hash = Math.imul(31, hash) + (input.codePointAt(index) ?? 0);
   }
 
-  return hash;
+  return Math.trunc(hash);
 }
