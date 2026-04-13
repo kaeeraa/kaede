c880e45b98b578dff71c2cc768eb4929a9c78c43
fractal <fractal@nebula-nook.ru>
2026-04-13 12:06:28 +0300

    feat: save code

--- src/components/settings/Settings.vue
+++ src/components/settings/Settings.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import PageWrapper from "@/components/general/layout/PageWrapper.vue";
+
+
 </script>
 
 <template>
   <PageWrapper>
-    Settings
+    <div
+      id="__settings-page__wrapper"
+      class="h-full w-full flex flex-col gap-2 py-2 pr-2"
+    >
+    </div>
   </PageWrapper>
 </template>
