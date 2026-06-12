81f901f2d5ed44c9a70de1a93d3f67e5e68a5c4a
fractal <fractal@nebula-nook.ru>
2026-06-12 22:19:16 +0300

    feat: add an initial settings code

--- src/components/settings/Settings.vue
+++ src/components/settings/Settings.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 import PageWrapper from "@/components/general/layout/PageWrapper.vue";
 
+const currentTab = ref<string>("general");
 
+function handleTab(tab: string): void {
+  currentTab.value = tab;
+}
 </script>
 
 <template>
@@ -10,6 +16,29 @@ import PageWrapper from "@/components/general/layout/PageWrapper.vue";
       id="__settings-page__wrapper"
       class="h-full w-full flex flex-col gap-2 py-2 pr-2"
     >
+      <div
+        id="__settings-page__tabs"
+        class="flex flex-wrap gap-2"
+      >
+        <button
+          id="__settings-page__tab-general"
+          @click="() => handleTab('general')"
+        >
+          General
+        </button>
+        <button
+          id="__settings-page__tab-discord-rpc"
+          @click="() => handleTab('discord-rpc')"
+        >
+          Discord RPC
+        </button>
+        <button
+          id="__settings-page__tab-plugin-playground"
+          @click="() => handleTab('plugin-playground')"
+        >
+          Plugin Playground
+        </button>
+      </div>
     </div>
   </PageWrapper>
 </template>
